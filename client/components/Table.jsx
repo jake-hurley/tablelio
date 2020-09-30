19fb5acbaf8bf17ae78810c6be07be2551d5e25a
Jake-Hurley <jshurley97@gmail.com>
2020-10-01 11:36:45 +1300

    passes mockdata through props to display table details

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -5,16 +5,20 @@ import Categories from './Categories'
 import Menu from './Menu'
 import TableDetails from './TableDetails'
 
+import Data from'../../data.json'
+
 // API CALL GATHERS ALL DATA AND PASSES IT DOWN THROUGH PROPS 5HEAD
+
+const mockData = Data.companies[0].tables[0]
 
 export class Table extends Component {
     render () {  
         return (
             <>
-                <TableDetails />
-                <Cart />
-                <Categories />
-                <Menu />
+                <TableDetails data={mockData}/>
+                <Cart data={mockData}/>
+                <Categories data={mockData}/>
+                <Menu data={mockData}/>
             </>
         )
     }
--- client/components/TableDetails.jsx
+++ client/components/TableDetails.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 class TableDetails extends React.Component {
     render () {
+        console.log(this.props.data)
+        const tableData = this.props.data
         return (
             <>
-                <h1>Table Number</h1>
+                <h1>{tableData.tableName}</h1>
                 <button>Log out</button>
-                <h3>Current Total</h3>
+                <h3>Current Total: {tableData.totalPrice}</h3>
             </>
         )
     }
